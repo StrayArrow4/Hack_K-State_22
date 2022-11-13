b7fba2c4aa704b4b10500a2de0f37b9d21b4a30f
KentDeterding <109356505+KentDeterding@users.noreply.github.com>
2022-11-13 05:44:45 -0600

    PWA prompt maybe

--- main.js
+++ main.js
@@ -233,13 +233,41 @@ const turnIndicator = document.getElementById('turnIndicator');
 const home = document.getElementById('home');
 const winScreen = document.getElementById('winScreen');
 const winMsg = document.getElementById('winMsg');
+const addBtn = document.querySelector(".add-button");
 
+let deferredPrompt;
 let coinsInColumn = [0, 0, 0, 0, 0, 0, 0];
 let moveQueue = [];
 let board = {red: 0n, blue: 0n};
 let currentTurn = 'red';
 
+addBtn.style.display = "none";
 
+window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = "block";
+  
+    addBtn.addEventListener("click", (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = "none";
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        deferredPrompt = null;
+      });
+    });
+});
+  
 buttons.forEach(button => {
     button.addEventListener('click', buttonHandler);
 })
