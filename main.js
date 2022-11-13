3f1a0ff46929e52c2769c2d71392becb42aa9e39
KentDeterding <109356505+KentDeterding@users.noreply.github.com>
2022-11-13 06:06:39 -0600

    New Icons

--- main.js
+++ main.js
@@ -255,15 +255,6 @@ window.addEventListener("beforeinstallprompt", (e) => {
       addBtn.style.display = "none";
       // Show the prompt
       deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("User accepted the A2HS prompt");
-        } else {
-          console.log("User dismissed the A2HS prompt");
-        }
-        deferredPrompt = null;
-      });
     });
 });
   
